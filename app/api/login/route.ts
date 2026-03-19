5f9d9cc0462d2a5a3a306a22901dded5b14cb201
czetxt <lesoca00@gmail.com>
2026-03-19 15:24:53 -0300

    fix auth secs

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: Request) {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
-      //maxAge: 60 * 60 * 24 * 30, // 30 days in seconds
+      maxAge: 60, // 30 days in seconds
       path: "/",
     });
 
